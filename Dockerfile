65096de035abd9582d3eca29fbd7f91e00184fbe
Mirela Neziri <mirelaneziri17@gmail.com>
2024-09-16 10:32:53 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM php:8.1-apache
+
+WORKDIR /var/www/html
+
+COPY . .
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+
---
+++ app/Models/Book.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class Book extends Model
+{
+    use HasFactory;
+// name of the relationship
+    public function reviews(){
+
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder {
+        return $query->where('title', 'LIKE', '%' . $title . '$'); 
+    }
+}
